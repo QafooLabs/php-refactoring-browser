e7c5be256732a0868681e174c79c26045569ecff
Benjamin Eberlei <kontakt@beberlei.de>
2013-03-24 12:57:22 +0100

    Refactored extract method code generation into EditingSession class.

--- src/main/QafooLabs/Refactoring/Application/ExtractMethod.php
+++ src/main/QafooLabs/Refactoring/Application/ExtractMethod.php
@@ -4,6 +4,8 @@ namespace QafooLabs\Refactoring\Application;
 
 use QafooLabs\Refactoring\Domain\Model\LineRange;
 use QafooLabs\Refactoring\Domain\Model\File;
+use QafooLabs\Refactoring\Domain\Model\EditingSession;
+
 use QafooLabs\Refactoring\Domain\Services\VariableScanner;
 use QafooLabs\Refactoring\Domain\Services\CodeAnalysis;
 use QafooLabs\Refactoring\Domain\Services\Editor;
@@ -43,61 +45,12 @@ class ExtractMethod
 
         $definedVariables = $this->variableScanner->scanForVariables($file, $range);
 
-        $methodCall = $this->generateMethodCall($newMethodName, $definedVariables, $isStatic);
-        $methodCode = $this->generateExtractedMethodCode($newMethodName, $selectedCode , $definedVariables, $isStatic);
-
         $buffer = $this->editor->openBuffer($file);
-        $buffer->replace($range, array($methodCall));
-        $buffer->append($extractedMethodEndsOnLine, $methodCode);
+
+        $session = new EditingSession($buffer);
+        $session->replaceRangeWithMethodCall($range, $newMethodName, $definedVariables, $isStatic);
+        $session->addMethod($extractedMethodEndsOnLine, $newMethodName, $selectedCode , $definedVariables, $isStatic);
 
         $this->editor->save();
-    }
-
-    private function generateMethodCall($newMethodName, $definedVariables, $isStatic)
-    {
-        $ws = str_repeat(' ', 8);
-        $argumentLine = $this->implodeVariables($definedVariables->getLocalVariables());
-
-        $code = $isStatic ? 'self::%s(%s);' : '$this->%s(%s);';
-        $call = sprintf($code, $newMethodName, $argumentLine);
-
-        if (count($definedVariables->getAssignments()) == 1) {
-            $call = '$' . $definedVariables->getAssignments()[0] . ' = ' . $call;
-        } else if (count($definedVariables->getAssignments()) > 1) {
-            $call = 'list(' . $this->implodeVariables($definedVariables->getAssignments()) . ') = ' . $call;
-        }
-
-        return $ws . $call;
-    }
-
-    private function generateExtractedMethodCode($newMethodName, $selectedCode, $definedVariables, $isStatic)
-    {
-        $ws = str_repeat(' ', 8);
-        $wsm = str_repeat(' ', 4);
-
-        if (count($definedVariables->getAssignments()) == 1) {
-            $selectedCode[] = '';
-            $selectedCode[] = $ws . 'return $' . $definedVariables->getAssignments()[0] . ';';
-        } else if (count($definedVariables->getAssignments()) > 1) {
-            $selectedCode[] = '';
-            $selectedCode[] = $ws . 'return array(' . $this->implodeVariables($definedVariables->getAssignments()) . ');';
-        }
-
-        $paramLine = $this->implodeVariables($definedVariables->getLocalVariables());
-
-        $methodCode = array_merge(
-            array('', $wsm . sprintf('private%sfunction %s(%s)', $isStatic ? ' static ' : ' ', $newMethodName, $paramLine), $wsm . '{'),
-            $selectedCode,
-            array($wsm . '}')
-        );
-
-        return $methodCode;
-    }
-
-    private function implodeVariables($variableNames)
-    {
-        return implode(', ', array_map(function ($variableName) {
-            return '$' . $variableName;
-        }, $variableNames));
     }
 }
--- src/main/QafooLabs/Refactoring/Domain/Model/EditingSession.php
+++ src/main/QafooLabs/Refactoring/Domain/Model/EditingSession.php
@@ -29,6 +29,53 @@ class EditingSession
         }
     }
 
+    public function replaceRangeWithMethodCall(LineRange $range, $newMethodName, $definedVariables, $isStatic)
+    {
+        $argumentLine = $this->implodeVariables($definedVariables->getLocalVariables());
+
+        $code = $isStatic ? 'self::%s(%s);' : '$this->%s(%s);';
+        $call = sprintf($code, $newMethodName, $argumentLine);
+
+        if (count($definedVariables->getAssignments()) == 1) {
+            $call = '$' . $definedVariables->getAssignments()[0] . ' = ' . $call;
+        } else if (count($definedVariables->getAssignments()) > 1) {
+            $call = 'list(' . $this->implodeVariables($definedVariables->getAssignments()) . ') = ' . $call;
+        }
+
+        $this->buffer->replace($range, array($this->whitespace(8) . $call));
+    }
+
+    public function addMethod($line, $newMethodName, $selectedCode, $definedVariables, $isStatic)
+    {
+        $ws = str_repeat(' ', 8);
+        $wsm = str_repeat(' ', 4);
+
+        if (count($definedVariables->getAssignments()) == 1) {
+            $selectedCode[] = '';
+            $selectedCode[] = $ws . 'return $' . $definedVariables->getAssignments()[0] . ';';
+        } else if (count($definedVariables->getAssignments()) > 1) {
+            $selectedCode[] = '';
+            $selectedCode[] = $ws . 'return array(' . $this->implodeVariables($definedVariables->getAssignments()) . ');';
+        }
+
+        $paramLine = $this->implodeVariables($definedVariables->getLocalVariables());
+
+        $methodCode = array_merge(
+            array('', $wsm . sprintf('private%sfunction %s(%s)', $isStatic ? ' static ' : ' ', $newMethodName, $paramLine), $wsm . '{'),
+            $selectedCode,
+            array($wsm . '}')
+        );
+
+        $this->buffer->append($line, $methodCode);
+    }
+
+    private function implodeVariables($variableNames)
+    {
+        return implode(', ', array_map(function ($variableName) {
+            return '$' . $variableName;
+        }, $variableNames));
+    }
+
     public function addProperty($line, $propertyName)
     {
         $this->buffer->append($line, array(
