f381cb39c1f24b1d662715e7151d9de8a0a3d8b6
Philipp Scheit <p.scheit@ps-webforge.com>
2013-06-29 08:58:06 +0200

    Model: add a UseStatement

---
+++ src/test/QafooLabs/Refactoring/Domain/Model/UseStatementTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace QafooLabs\Refactoring\Domain\Model;
+
+class UseStatementTest extends \PHPUnit_Framework_TestCase
+{
+    private $useStatement;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $file = File::createFromPath(__FILE__, __DIR__);
+        $this->useStatement = new UseStatement($file, LineRange::fromLines(3,5));
+    }
+
+    public function testReturnsEndLineFromLineRange() {
+        $this->assertEquals(5, $this->useStatement->getEndLine());
+    }
+}
